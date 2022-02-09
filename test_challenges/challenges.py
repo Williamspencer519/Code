013739b5e206a63166212374583aea043ad24f2f
William Spencer <william@Williams-MacBook-Pro-3.local>
2022-02-09 09:47:14 -0500

    fixeed a typo

--- test_challenges/challenges.py
+++ test_challenges/challenges.py
@@ -14,7 +14,7 @@ def bridges(islands, bridges):
 	# this is an implementation of the Bridges of Konigsberg problem.  Don't be intimidated by this one
 	# if you don't just think of an answer, it's legitimately a hard problem, it's a problem
 	# Euler figured out over a few days, which would mean it would be pretty ridiculous to expect
-	# a mortal to do it, except for the fact you get somethign Euler didn't have: A computer
+	# a mortal to do it, except for the fact you get something Euler didn't have: A computer
 
 	# It goes like this.  There are 4 islands, and seven bridges, each bridge connecting some island to another
 	# can you write a program to verify if it's possible to walk these bridges, each once, but NEVER more than once
