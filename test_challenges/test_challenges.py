48c72aefc853f0029ff0baa55a2202205214b81e
William Spencer <william@Williams-MacBook-Pro-3.local>
2022-02-09 09:46:30 -0500

    fixeed a typo

--- test_challenges/test_challenges.py
+++ test_challenges/test_challenges.py
@@ -17,6 +17,6 @@ def test_bridges():
 	bridges = [ ('a','b'), ('a', 'b'), ('a','c'), ('a','c'), ('a','d'), ('b','d'), ('c','d')]
 	# Note that there are duplicates in the edges, there are some islands that have 2 distinct bridges 
 	# between them.  If you look here it's this setup https://en.wikipedia.org/wiki/Seven_Bridges_of_K%C3%B6nigsberg
-	# though don't read too much, because it contains the veery clever answer!
+	# though don't read too much, because it contains the very clever answer!
 	assert not bridges(islands, bridges) #Spoiler, you can't!
-	
+
